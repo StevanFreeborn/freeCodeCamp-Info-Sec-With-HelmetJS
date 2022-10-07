fd86105b64695e87c48259c332cd7f81c43057ac
Stevan Freeborn <sfreeborn7@gmail.com>
2022-10-07 13:48:58 -0500

    added hsts middleware

--- myApp.js
+++ myApp.js
@@ -7,6 +7,9 @@ app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 
+const ninetyDaysInSeconds = 90*24*60*60;
+
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 
 
 
