9de88dd383b48fbb37b65c61e34f6d884c0cbf23
Stevan Freeborn <sfreeborn7@gmail.com>
2022-10-07 13:46:10 -0500

    add ieNoOpen middleware

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.noSniff());
-app.use(ieNoOpen());
+app.use(helmet.ieNoOpen());
 
 
 
